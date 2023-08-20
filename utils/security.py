759fc15331b50e8b1be5a487fa49eecebebeb0cf
Sulav Maharjan <sulavmaharjan63@gmail.com>
2023-08-20 11:03:07 +0545

    :necktie: raise proper exceptions

--- utils/security.py
+++ utils/security.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 
@@ -12,13 +12,22 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/access-token")
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     """Get the current user."""
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
     try:
         payload = jwt.decode(token=token, key=settings.JWT_SECRET, algorithms=["HS256"])
         email = payload.get("sub")
-        user = await User.get_user_by_email(email=email)
-        return user or None
-    except JWTError as err:
-        raise err
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = await User.get_user_by_email(email=email)
+    if user is None:
+        raise credentials_exception
+    return user
 
 
 def create_access_token(sub: str):
